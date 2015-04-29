63ce9695633632e7ac8209dd8fb2af842bcaedc1
Paul Buser <buser.paul@gmail.com>
2015-04-29 18:29:47 -0400

    Working on BFS implementation. Things broke.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ The dream lives on... that one day you too will be a champion whiteboard roller 
 This is a game about territory control. Players move in 4 orthogonal directions on a large grid. Each player moves one tick per turn. If a player completes a filled-in shape (definition of "filled-in" TBD), they are granted the filled-in area as captured! Whoever has the most captured area after (the whole board is captured | a timer runs out) is the winner!
 
 ## Dependencies ##
-- python 3
+- python
+- pygame
 - sense of humor
 - ability to suspend disbelief
--- board.py
+++ board.py
@@ -14,7 +14,8 @@ class Board(object):
     wall_char = 'A'
     empty_char = 'x'
     trail_char = 't'
-    colors = { wall_char: Color('Green'), empty_char: Color('White'), trail_char: Color('firebrick'), '1': Color('Red'), '2': Color('Blue') }
+    captured_char = 'C'
+    colors = { wall_char: Color('black'), empty_char: Color('White'), trail_char: Color('green'), captured_char: Color('pink')}
     def __init__(self, filename, player_list):
         self.board = []
         self.width = 0
@@ -52,46 +53,37 @@ class Board(object):
 
     """ Returns a list of indices of neighbors of a node in our board
         Keyword arguments:
+        coords -- a coordinate to get neighbors of
         filt -- a function that takes a Node as a single parameter and returns True or False
         """
-    def get_neighbors(self, coords, filt=None):
+    def _get_neighbors(self, coords, filt):
         L = []
         x, y = coords
         # define a list for North, South, East, West
         for neighbor_x, neighbor_y in [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]:
             if neighbor_x >= 0 and neighbor_x < self.width and neighbor_y >= 0 and neighbor_y < self.height:
-                if filt and filt(self[neighbor_x, neighbor_y]):
-                    L.append((neighbor_x, neighbor_y))
-                elif filt is None:
+                if filt(self[neighbor_x, neighbor_y]):
                     L.append((neighbor_x, neighbor_y))
         return L
 
     """ Returns True or False depending on whether the coordinates are on the edge of the board"""
-    def is_edge(self, coords):
+    def _is_edge(self, coords):
         x, y = coords
         return x == 0 or y == 0 or x == self.width-1 or y == self.height-1
 
-    """ Returns a flat list of nodes in the board
-        Syntax from: http://stackoverflow.com/questions/952914/making-a-flat-list-out-of-list-of-lists-in-python
-    """
-    def __iter__(self):
-        return iter([item for sublist in self.board for item in sublist])
-
     """ Sets all nodes on the board as Unexplored """
-    def set_all_unexplored(self):
-        for node in self:
-            node.explored = False
+    def _set_all_unexplored(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                self[x, y].explored = False
 
     """ Does a BFS at a point in the board.
-        If it finds the edge, sets all nodes in the area to open_char.
-        Otherwise, sets all nodes in the area to closed_char
+        Returns a tuple (is_open, seen_list) of whether the point is open, and coordinates of points that are accessible from the starting coordinate
         @param coords - the coordinates of the starting position of the BFS
         @param filt - a boolean function that takes a Node as its single parameter for filtering finding neighbors
         @return void"""
-    def test_closed(self, coords, filt, open_char, closed_char):
-        # If we filter the starting coordinates, do nothing
-        if not filt(self[coords]):
-            return
+    def _check_open(self, coords, filt):
+        self._set_all_unexplored()
         seen_list = []
         work_queue = deque()
         is_open = False
@@ -100,16 +92,11 @@ class Board(object):
             curnode_coords = work_queue.popleft()  # pop current node off our queue
             self[curnode_coords].explored = True  # set node as explored
             seen_list.append(curnode_coords)   # add it to our seen list
-            if self.is_edge(curnode_coords):   #if it's an edge, we know the entire area is open
+            if self._is_edge(curnode_coords):   #if it's an edge, we know the entire area is open
                 is_open = True
-            for neighbor_coords in self.get_neighbors(curnode_coords, filt):  # add our (filtered) neighbors to the queue
+            for neighbor_coords in self._get_neighbors(curnode_coords, filt):  # add our (filtered) neighbors to the queue
                 work_queue.append(neighbor_coords)
-        if is_open:
-            self.set_to(seen_list, open_char)
-            print("Found edge of the board!")
-        else:
-            self.set_to(seen_list, closed_char)
-            print("Didn't find edge of the board.")
+        return is_open, seen_list
 
     """ Sets a list of coords of nodes in the board to be a particular state"""
     def set_to(self, coords_list, state):
@@ -141,9 +128,12 @@ class Board(object):
 
         # actually move the players
         for player in self.player_list:
-            self[player.pos].state = player.char # write trail where player was
+            self[player.pos].state = Board.trail_char  # write trail where player was
             player.pos = player.intended_pos
             self[player.pos].state = player.char
+
+    def _search_filt(self, node):
+        return node.state != Board.wall_char and node.state != Board.trail_char and node.state not in (player.char for player in self.player_list) and not node.explored
 
     def process_input(self, pressed_keys):
         for player in self.player_list:
@@ -151,19 +141,25 @@ class Board(object):
 
     def update(self):
         self.move_players()
+        for player in self.player_list:
+            is_open, seen_coords = self._check_open(player.pos, self._search_filt)
+            if not is_open:
+                self.set_to(seen_coords, Board.captured_char)
 
     def render(self, screen, pixel_size):
         # draw our walls and empty spaces
         for y in range(self.height):
             for x in range(self.width):
-                if self[x, y].state in Board.colors:
-                    draw_color = Board.colors[self[x, y].state]
+                char = self[x, y].state
+                draw_color = Color('Black') # default color
+                if char in Board.colors:
+                    draw_color = Board.colors[char]
                 else:
-                    draw_color = Color('Black')
+                    for player in self.player_list:
+                        if char == player.char:
+                            draw_color = player.color
+                            break
                 screen.fill(draw_color, Rect(x * pixel_size, y * pixel_size, pixel_size, pixel_size))
-        # draw our players
-        for player in self.player_list:
-            screen.fill(player.color, Rect(player.pos[0] * pixel_size, player.pos[1] * pixel_size, pixel_size, pixel_size))
 
 if __name__ == "__main__":
     board = Board('test_board.brd')
